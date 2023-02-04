ac76c31c0ecbf48707fb8595a773d50a8073d675
Stevesx <121517229+Stevesx@users.noreply.github.com>
2023-02-03 23:18:57 -0300

    commit inicial
    surpresa

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("myButton").addEventListener("click", function(){
+    alert("Essa opção esta inválida");
+  });
+  document.getElementById("myButtonn").addEventListener("click", function(){
+    alert("Opção correta EU TE AMO MUITO");
+  });
+  
+  
